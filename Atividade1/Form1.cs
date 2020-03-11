45f20be3f98ef1ba9d7c1a47541dc42266ec8e81
Fernando Bonfim Andrade <fernandobonfimandrade@gmail.com>
2020-03-11 09:53:07 -0300

    add files
    ok

---
+++ Atividade1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo_volume_Cilindro
+{
+    public partial class volume : Form
+    {
+        public volume()
+        {
+            InitializeComponent();
+        }
+
+        private void calcular_Click(object sender, EventArgs e)
+        {
+            double calc, r, h;
+            if (double.TryParse(raio.Text, out r) && double.TryParse(altura.Text, out h))
+            {
+                calc = Math.PI * Math.Pow(r, 2) * h;
+                resultado.Text = calc.ToString("N2");
+            }
+            else
+            {
+                MessageBox.Show("Operadores Inválidos!");
+            }
+        }
+
+
+    }
+}
