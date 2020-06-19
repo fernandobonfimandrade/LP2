f2ff422802da1534583891a64ae87df658e5bc4c
Fernando Bonfim Andrade <fernandobonfimandrade@gmail.com>
2020-06-19 20:11:11 -0300

    prova
    ok

---
+++ Prova/P30481923015/P30481923015/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace P30481923015
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EntradaDados = new System.Windows.Forms.Button();
+            this.BoxSaida = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // EntradaDados
+            // 
+            this.EntradaDados.Location = new System.Drawing.Point(156, 184);
+            this.EntradaDados.Name = "EntradaDados";
+            this.EntradaDados.Size = new System.Drawing.Size(125, 60);
+            this.EntradaDados.TabIndex = 0;
+            this.EntradaDados.Text = "Informar Vendas";
+            this.EntradaDados.UseVisualStyleBackColor = true;
+            this.EntradaDados.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BoxSaida
+            // 
+            this.BoxSaida.FormattingEnabled = true;
+            this.BoxSaida.Location = new System.Drawing.Point(434, 23);
+            this.BoxSaida.Name = "BoxSaida";
+            this.BoxSaida.Size = new System.Drawing.Size(344, 394);
+            this.BoxSaida.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BoxSaida);
+            this.Controls.Add(this.EntradaDados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EntradaDados;
+        private System.Windows.Forms.ListBox BoxSaida;
+    }
+}
+
---
+++ Prova/P30481923015/P30481923015/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic; 
+
+namespace P30481923015
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[,] Vetor = new double[10,4];
+            string entrada = "";
+            int sai = 0;
+            double aux;
+            string saida = "";
+            double somaMes = 0.00;
+            double somaTotal = 0.00;
+            BoxSaida.Items.Clear();
+            for (var x = 0; x < 10; x++) 
+            {
+                somaMes = 0.00;
+                for (var y = 0; y < 4; y++)
+                {
+                    entrada = Interaction.InputBox("Digite um valor da venda para Mes " + (x + 1)+ " Semana " + (y + 1),"Entrada de Dados");
+                    if(entrada == "")
+                    {
+                        sai = 1;
+                        break;
+                    }
+                    if (!Double.TryParse(entrada, out aux))
+                    {
+                        MessageBox.Show("Digite número válido!");
+                        y--;
+                    }
+                    else
+                    {
+                        Vetor[x, y] = aux;
+                        somaMes += aux;
+                        saida = "Total vendido Mes : "+(x+1)+" Semana: "+(y+1)+" R$ "+ aux.ToString("N2") + "\n";
+                        BoxSaida.Items.Add(saida);
+                    }
+                }
+                if (sai == 1)
+                {
+                    break;
+                }
+                somaTotal += somaMes;
+                saida = " >> Total vendido Mes R$ " + somaMes.ToString("N2");
+                BoxSaida.Items.Add(saida);
+                BoxSaida.Items.Add("---------------------");
+            }
+            saida = " >>>>>>>Total Geral R$ " + somaTotal.ToString("N2");
+            BoxSaida.Items.Add(saida);
+        }
+
+    }
+}
